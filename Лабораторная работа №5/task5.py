b2789902949d59e84bc5a0d43ff56c7dab0cb838
AlinaLuninaVyacheslavovna <118982783+AlinaLuninaVyacheslavovna@users.noreply.github.com>
2022-12-23 21:42:47 +0300

    Update task5.py

--- Лабораторная работа №5/task5.py
+++ Лабораторная работа №5/task5.py
@@ -2,8 +2,7 @@ from random import sample
 from string import ascii_lowercase, ascii_uppercase, digits
 
 
-def get_random_password() -> str:
-    n = 8
+def get_random_password(n=8) -> str:
     password = "".join(sample(ascii_uppercase + ascii_lowercase + digits, n))
     return password
 
