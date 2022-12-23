eef16d870f55e452f5fb9ca037bd9060200b4c4e
AlinaLuninaVyacheslavovna <118982783+AlinaLuninaVyacheslavovna@users.noreply.github.com>
2022-12-23 21:20:53 +0300

    Update task3.py

--- Лабораторная работа №5/task3.py
+++ Лабораторная работа №5/task3.py
@@ -2,15 +2,12 @@ from random import randint
 
 
 def get_unique_list_numbers(start=-10, stop=10, count=15) -> list[int]:
+    random_digit = []
+    while len(random_digit) < count:
+        num_ = randint(start, stop)
+        if num_ not in random_digit:
+            random_digit.append(num_)
 
-    random_digit = [randint(start, stop)]
-
-    for digit in range(count):
-
-        if digit not in random_digit:
-            random_digit.append(digit)
-        else:
-            randint(start, stop)
     return random_digit
 
 
